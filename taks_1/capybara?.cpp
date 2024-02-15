510be87f34c0baa53d835316387bb2cfa913ad34
Erykalkin <60609438+Erykalkin@users.noreply.github.com>
2024-02-15 19:36:30 +0300

    Update capybara?.cpp

--- taks_1/capybara?.cpp
+++ taks_1/capybara?.cpp
@@ -1,1 +1,23 @@
+import gmsh
+import sys
 
+
+gmsh.initialize()
+
+gmsh.merge(R"C:\Users\George.LAPTOP-TLP259VH\Desktop\d20.stl")
+n = gmsh.model.getDimension()
+s = gmsh.model.getEntities(n)
+l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
+
+gmsh.model.geo.addVolume([l])
+gmsh.model.geo.synchronize()
+
+gmsh.model.mesh.generate(3)
+
+gmsh.write("t2.msh")
+gmsh.write("t2.geo_unrolled")
+
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
+
+gmsh.finalize()
