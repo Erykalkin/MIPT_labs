f4e456aacf94b0e2c1778a73e3455ff7f2096bf7
Erykalkin <60609438+Erykalkin@users.noreply.github.com>
2024-02-29 17:35:13 +0300

    Update capybara?.py

--- taks_1/capybara?.py
+++ taks_1/capybara?.py
@@ -4,7 +4,12 @@ import sys
 
 gmsh.initialize()
 
-gmsh.merge(R"C:\Users\George.LAPTOP-TLP259VH\Desktop\d20.stl")
+gmsh.model.add("stl")
+gmsh.merge(R"C:\Users\George.LAPTOP-TLP259VH\Desktop\Прога\fish.stl")
+gmsh.model.mesh.classifySurfaces(1, True, True)
+gmsh.model.mesh.createGeometry()
+#gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 1.0)
+
 n = gmsh.model.getDimension()
 s = gmsh.model.getEntities(n)
 l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
